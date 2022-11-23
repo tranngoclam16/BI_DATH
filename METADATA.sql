e506fe15a248f1dca8bf03323e7f7d8210e4a7f4
nkttn72 <nkttnga@gmail.com>
2022-11-23 10:59:11 +0700

    Create METADATA.sql

---
+++ METADATA.sql
@@ -0,0 +1,15 @@
+USE [METADATA]
+GO
+
+INSERT INTO [dbo].[Data_Flow]
+           (TenBang, LSET)
+     VALUES
+           ('CasesReport', '1990-01-01'),
+		   ('CompileCovid19CaseDetailsCanada', '1990-01-01'),
+		   ('OngoingOutbreaksPHU', '1990-01-01'),
+		   ('PublicHealthUnit', '1990-01-01'),
+		   ('PublicHealthUnitsGroup', '1990-01-01'),
+		   ('VaccinesByAgePHU', '1990-01-01')
+GO
+
+
