b168fe2bf45c6b98db8875cb7559b9a02a7a8747
nkttn72 <nkttnga@gmail.com>
2022-11-23 11:19:59 +0700

    Update METADATA.sql

--- METADATA.sql
+++ METADATA.sql
@@ -1,3 +1,32 @@
+CREATE DATABASE [METADATA]
+GO
+
+USE [METADATA]
+GO
+
+/****** Object:  Table [dbo].[Data_Flow]    Script Date: 11/23/2022 11:10:14 AM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Data_Flow]') AND type in (N'U'))
+DROP TABLE [dbo].[Data_Flow]
+GO
+
+/****** Object:  Table [dbo].[Data_Flow]    Script Date: 11/23/2022 11:10:14 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Data_Flow](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[TenBang] [varchar](50) NULL,
+	[LSET] [datetime] NULL,
+ CONSTRAINT [PK_Data_Flow] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 USE [METADATA]
 GO
 
