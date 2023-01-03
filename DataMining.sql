5bbb19c6e735c1a25cf817fd5b825c30ab5ad51d
tranngoclam16 <63225101+tranngoclam16@users.noreply.github.com>
2023-01-03 13:19:41 +0700

    mining

--- DataMining.sql
+++ DataMining.sql
@@ -52,14 +52,15 @@ FROM FactCaseDetail F
 	JOIN DimOutcome DO ON DO.OutcomeID = F.OutcomeID
 	JOIN DimSeverity DS ON DS.SeverityID = F.SeverityID
 
-
+-- Create table for mining and visualize
+INSERT INTO miningCaseDetail
 SELECT 
 	ROW_NUMBER() OVER (ORDER BY F.PHUID, F.DateID, F.GenderID) AS ID
 	, DP.PHUName
 	, DP.CityName
 	, DP.GroupName
 	, DD.Date
-	,'Q' + CAST(DD.Quarter AS varchar) + '/' + CAST(DD.Year AS varchar) AS TPeriod
+	,'Q' + CAST(DD.Quarter AS varchar) + '/' + CAST(DD.Year AS varchar) AS TimePeriod
 	, DD.Year
 	, DG.GenderID
 	, DG.Gender
@@ -70,7 +71,9 @@ SELECT
 	, DO.OutcomeID
 	, DO.Outcome
 	, DS.SeverityID
-	, DS.Severity into miningCaseDetail
+	, DS.Severity 
+	, 0 as Label1
+	, 0 as Label2 
 FROM FactCaseDetail F
 	JOIN DimPHU DP ON DP.PHUID_SK = F.PHUID
 	JOIN DimDate DD ON DD.DateID = F.DateID
@@ -80,3 +83,52 @@ FROM FactCaseDetail F
 	JOIN DimOutcome DO ON DO.OutcomeID = F.OutcomeID
 	JOIN DimSeverity DS ON DS.SeverityID = F.SeverityID
 
+SELECT *
+FROM miningCaseDetail
+where ID=1
+
+UPDATE m
+SET m.Label1 = p.Cluster
+FROM miningCaseDetail as m inner join (select ID, [$CLUSTER] as Cluster from openquery([BI_DATH] , '
+									SELECT    
+										  t.[ID],   
+										  Cluster() 
+									FROM  
+										[Clustering]
+									NATURAL PREDICTION JOIN  
+										OPENQUERY([Covid19],  
+										''SELECT 
+											ID,
+											Age,
+											Gender,
+											Outcome,
+											TimePeriod
+										FROM miningCaseDetail
+									'') AS t  ')) as p on m.ID=p.ID
+
+go
+CREATE PROCEDURE sp_AssignClusterLabel
+AS
+	select ID, [$CLUSTER] as Cluster INTO #TEMPTABLE
+	from openquery([BI_DATH] , '
+									SELECT    
+										  t.[ID],   
+										  Cluster() 
+									FROM  
+										[Clustering]
+									NATURAL PREDICTION JOIN  
+										OPENQUERY([Covid19],  
+										''SELECT 
+											ID,
+											Age,
+											Gender,
+											Outcome,
+											TimePeriod
+										FROM miningCaseDetail
+									'') AS t  ')
+
+	UPDATE m
+	SET m.Label1 = p.Cluster
+	FROM miningCaseDetail as m inner join #TEMPTABLE as p on m.ID=p.ID
+GO
+SELECT * FROM miningCaseDetail
